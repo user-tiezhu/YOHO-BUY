a38baf9da62b72c1430ec474f10c30df01d31cbb
YOHO <1355803345@qq.com>
2019-12-18 21:03:22 +0800

    第三次提交

--- CYQ/src/script/lunbo.js
+++ CYQ/src/script/lunbo.js
@@ -7,21 +7,53 @@ class Lunbo {
         this.right = document.querySelector('.banner-t .gt');
         this.num = 0; //当前操作的按钮的索引。
         this.timer = null;
+
     }
     init() {
         let _this = this;
+        //右箭头
         this.right.onclick = function () {
             _this.num++;
-            if(_this.num>7){
-                _this.num=1;
+            if (_this.num > 7) {
+                _this.num = 0;
             }
-            for (let i = 0; i <8; i++) {
-                _this.picli[i].className=""
+            for (let i = 0; i < 8; i++) {
+                _this.picli[i].className = "";
+                _this.btnli[i].className = "";
             }
-            _this.picli[_this.num].className="show"           
+            _this.picli[_this.num].className = "show";
+            _this.btnli[_this.num].className = "focus";
         }
+        //左箭头
+        this.left.onclick = function () {
+            _this.num--;
+            if (_this.num < 0) {
+                _this.num = 7;
+            }
+            for (let i = 0; i < 8; i++) {
+                _this.picli[i].className = "";
+                _this.btnli[i].className = "";
+            }
+            _this.picli[_this.num].className = "show";
+            _this.btnli[_this.num].className = "focus";
+            console.log(_this.num)
+        }
+        //定时轮播
+        this.timer = setInterval(function () {
+            _this.num++;
+            for (let i = 0; i < 8; i++) {
+                _this.picli[i].className = "";
+                _this.btnli[i].className = "";
+            }
+            if (_this.num > 7) {
+                _this.num = 0;
+            }
+            _this.picli[_this.num].className = "show";
+            _this.btnli[_this.num].className = "focus";
+        }, 2000)
     }
-    
-
 }
+
+
+
 new Lunbo().init();
---
+++ CYQ/src/script/render.js
@@ -0,0 +1,34 @@
+
+
+    class Render {
+        constructor() {
+            this.goodslist = document.querySelector('.goodslist');
+        }
+
+        init() {
+            let _this=this;
+            ajax({
+                url: 'http://10.31.161.198/YOHO-BUY/CYQ/php/yohodata.php',
+                dataType:'json'
+            }).then(function (data) {
+                console.log(data);
+                //遍历render
+                let strhtml='<ul>';
+                for(let value of data){
+                    strhtml+=`
+                    <li>
+                    <a href="details.html?sid=${value.sid}">
+                        <img src="${value.url}">
+
+                    </a>
+                </li>
+                    `;
+                }
+                strhtml+='</ul>';
+                _this.goodslist.innerHTML=strhtml;
+            });
+        }
+
+    }
+
+    new Render().init();
